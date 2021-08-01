bd988aece2472f822be1b194aaa364684b80dfc0
sandesh shrestha <sandeshshrestha63@gmail.com>
2021-08-01 22:16:42 +0545

    changes for user login and addto cart for individual product and logo

--- e-commerce/e-commerce/Controllers/AccountController.cs
+++ e-commerce/e-commerce/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using e_commerce.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,31 @@ namespace e_commerce.Controllers
 {
     public class AccountController : Controller
     {
+        SimplyShoppingEntities db = new SimplyShoppingEntities();
         // GET: Account
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Tbl_Members tbl_Members)
+        {
+            bool isValid = db.Tbl_Members.Any(x => x.EmailId == tbl_Members.EmailId&& x.Password  == tbl_Members.Password  && x.IsActive== true && x.IsDelete == false);
+            if(isValid)
+            {
+                Session["UserName"] = tbl_Members.EmailId;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("Login");
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session["UserName"] = null;
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- e-commerce/e-commerce/Controllers/ProductController.cs
+++ e-commerce/e-commerce/Controllers/ProductController.cs
@@ -26,5 +26,58 @@ namespace e_commerce.Controllers
             item.Product = tbl_Product;
             return View(item);
         }
+        public ActionResult AddToCart(Item item)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = db.Tbl_Product.Find(item.Product.ProductId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = item.Quantity
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                List<Item> newcart = new List<Item>();
+                var product = db.Tbl_Product.Find(item.Product.ProductId);
+                var i = 0;
+                foreach (var data in cart)
+                {
+                    if (data.Product.ProductId == item.Product.ProductId)
+                    {
+                        int prevQty = data.Quantity;
+                        //cart.Remove(item);
+                        newcart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + item.Quantity
+                        });
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        newcart.Add(new Item()
+                        {
+                            Product = data.Product,
+                            Quantity = data.Quantity
+                        });
+                    }
+                }
+                if (i == 0)
+                {
+                    newcart.Add(new Item()
+                    {
+                        Product = product,
+                        Quantity = item.Quantity
+                    });
+                }
+                Session["cart"] = newcart;
+            }
+            return RedirectToAction("Index","Home");
+        }
     }
 }
